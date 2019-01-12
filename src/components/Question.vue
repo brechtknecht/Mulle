197a9db939a4f7cb909deddcde49a0392d2c56c0
Felix Tesche <felix.tesche@gmail.com>
2019-01-12 19:49:10 +0100

    Fixed flow

--- src/components/Question.vue
+++ src/components/Question.vue
@@ -1,29 +1,29 @@
 <template>
-    <div :class="{filter: isFilter}" class="question fullscreen">
-    <transition name="slide-fade" mode="out-in" appear>
+  <div :class="{filter: isFilter}" class="fullscreen">
+    <div v-if="state != 'question'">
+      <transition name="fade" mode="out-in" appear>
+        <QuestionGUI :question_id=currentQuestion._id :key=currentQuestion._id />
+      </transition>
+    </div>
+    <transition name="slide-fade" mode="out-in" appear>    
       <div v-if="state == 'question'" class="content" key="question">
         <h2 class="light"> Frage {{ currentQuestion._id }} </h2>
         <h3 class="light"> {{ currentQuestion.question }} </h3>
         <p class="light">  {{ currentQuestion.description }} </p>
         <button @click="nextStep" class="light"> <span> START </span> </button>
       </div>
-      <div v-if="state == 'answering'" class="content" key="question">
+      <div v-if="state == 'answering'" class="content" key="answering">
         <h2 class="light">Versuche mit dem Slider einzuschätzen vieviel jeder Deutsche am Tag wegwirft</h2>
 
-        <transition name="fade" appear>
-          <QuestionGUI :question_id=currentQuestion._id />
-        </transition>
 
         <button @click="nextStep" class="light"> <span> WEITER </span> </button>
       </div>
-      <div v-if="state == 'answer'" class="content" key="question">
+      <div v-if="state == 'answer'" class="content" key="answer">
         <h3 class="light"> {{ currentQuestion.answer.string }} </h3>
         <h4 class="light"> !!Deine Antwort: 400g!! </h4>
 
-        <QuestionGUI :question_id=currentQuestion._id />
-
-        <button @click="endQuestion" class="light"> <span> WEITER </span> </button>
-      </div>
+        <button @click="endQuestion" class="light"> <span> NÄCHSTE FRAGE </span> </button>
+      </div>      
     </transition>
   </div>
 </template>
@@ -61,9 +61,6 @@ export default {
       console.log("Next Step 👍");
       let currentState = this.sequence.indexOf(this.state);
       this.state = this.sequence[currentState + 1];
-
-      //Enables and disables B/W Filter
-      if(currentState >= 0) { this.isFilter = true; } else { this.isFilter = false; }
     },
     endQuestion () {
         this.$store.commit('INCREMENT_CURRENT_QUESTION');
@@ -76,6 +73,7 @@ export default {
 <style lang="scss">
   .fade-enter-active, .fade-leave-active {
     transition: opacity .5s;
+    transition-delay: 1.2s;
   }
   .fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
     opacity: 0;
--- src/views/Question-Template.vue
+++ src/views/Question-Template.vue
@@ -16,6 +16,7 @@ export default {
       return this.$store.getters.currentQuestion;
     }
   }
+
 }
 </script>
 
